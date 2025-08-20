1e389c5c91fa9a8a14cf500319633b8c25ccf346
exibozzz2 <masaradulovic96@gmail.com>
2025-08-20 17:34:52 +0200

    # PHP Plus - Image upload # Create corresponding route for view "upload image" blade # Create corresponding route for transfer uploaded image to TMP folder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UploadImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::view('/upload', 'upload');
+
+Route::controller(UploadImageController::class)->prefix("image")->name("image.")->group(function(){
+    Route::post('/upload', 'upload')->name('upload');
 });
