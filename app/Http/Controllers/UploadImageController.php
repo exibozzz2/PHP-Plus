acd5cc4916fb6663791d2bf47e046f606ae72601
exibozzz2 <masaradulovic96@gmail.com>
2025-08-22 13:58:00 +0200

    # PHP Plus - Image upload Create controller function that:
    # Check is upload image forwarded
    # Limit size of upload image to 2MB
    # Limit dimensions of uploaded image to 1920px x 1024px
    # Set and limit allowed type extensions
    # Generate unique image name by timestamp + extension
    # Check does folder exists, if doesn't create new one
    # Set path to transfer image to folder from temporary folder

--- app/Http/Controllers/UploadImageController.php
+++ app/Http/Controllers/UploadImageController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -9,7 +8,51 @@ class UploadImageController extends Controller
 {
     public function upload(Request $request): Response {
 
-        dd($_FILES);
+
+        if( !isset($_FILES['uploadImage']) ) {
+            die("Morate upload-ovati profilnu sliku");
+        }
+
+        $imageSize = $_FILES['uploadImage']['size'];
+
+        $maxImageSize = 2 * 1024 * 1024;
+
+        if($imageSize > $maxImageSize) {
+            die("Slika je prevelika!");
+        }
+
+        list($width, $height) = getimagesize($_FILES['uploadImage']['tmp_name']);
+
+        if($width > 1920 || $height > 1024) {
+            die("Maksimalna dimenzija slike moze biti 1920px x 1024px");
+        }
+
+        $allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+
+        $imageType = pathinfo($_FILES['uploadImage']['name'], PATHINFO_EXTENSION);
+
+        if( !in_array($imageType, $allowedExtensions)) {
+            die("Format slike mora biti: ".implode(',' , $allowedExtensions));
+        }
+
+        $imageName = time().".".$imageType;
+
+        $finalPath = "./uploads/$imageName";
+
+        $tmpFileName = $_FILES['uploadImage']['tmp_name'];
+
+        if( !is_dir('./profile-images') ) {
+            mkdir('./profile-images', 0755, true);
+        }
+
+        $uploadImage = move_uploaded_file($tmpFileName, $finalPath);
+
+        if($uploadImage) {
+            die("Uspesno ste upload-ovali sliku.");
+        }
+        else {
+            die("Neuspesno upload-ovanje slike.");
+        }
     }
 
 
